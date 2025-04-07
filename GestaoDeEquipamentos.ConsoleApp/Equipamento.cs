f7fecdf13931eea32306638f71e4c321898a1e47
davidinovargas <davidinovargas@gmail.com>
2025-04-07 08:24:55 -0300

    Adicionado construtor de Equipamento

--- GestaoDeEquipamentos.ConsoleApp/Equipamento.cs
+++ GestaoDeEquipamentos.ConsoleApp/Equipamento.cs
@@ -8,5 +8,18 @@ namespace GestaoDeEquipamentos.ConsoleApp
 {
     class Equipamento
     {
+        public int Id;
+        public string Nome;
+        public string Fabricante;
+        public decimal PrecoAquisicao;
+        public DateTime DataFabricacao;
+
+        public Equipamento(string nome, string fabricante, decimal precoAquisicao, DateTime dataFabricacao)
+        {
+            Nome = nome;
+            Fabricante = fabricante;
+            PrecoAquisicao = precoAquisicao;
+            DataFabricacao = dataFabricacao;
+        }
     }
 }
