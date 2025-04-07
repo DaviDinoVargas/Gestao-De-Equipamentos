b6c35815f8b6ff68102444007a32cef867340c0e
davidinovargas <davidinovargas@gmail.com>
2025-04-07 08:23:14 -0300

    Adicionado classes de Equipamento e Tela equipamento, criado switch de escolhas do usuário

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TelaEquipamento telaEquipamento = new TelaEquipamento();
+
+            while (true)
+            {
+                string opcaoEscolhida;
+
+                switch (opcaoEscolhida)
+                {
+                    case "1":
+                      
+                        break;
+
+                    case "2":
+
+                        break;
+
+                    case "3":
+
+                        break;
+
+                    case "4":
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Saindo do programa...");
+                        break;
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
