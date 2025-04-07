9d5215116c888b29407ea2ed378fe1e6dd174fe4
davidinovargas <davidinovargas@gmail.com>
2025-04-07 08:25:54 -0300

    Adicionado visualização do menu usuário

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -8,7 +8,7 @@
 
             while (true)
             {
-                string opcaoEscolhida;
+                string opcaoEscolhida = telaEquipamento.ApresentarMenu();
 
                 switch (opcaoEscolhida)
                 {
--- GestaoDeEquipamentos.ConsoleApp/TelaEquipamento.cs
+++ GestaoDeEquipamentos.ConsoleApp/TelaEquipamento.cs
@@ -8,5 +8,27 @@ namespace GestaoDeEquipamentos.ConsoleApp
 {
     class TelaEquipamento
     {
+        public Equipamento[] equipamentos = new Equipamento[100];
+        public int contadorEquipamentos = 0;
+
+        public string ApresentarMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Gestão de Equipamentos");
+            Console.WriteLine("--------------------------------------------");
+
+            Console.WriteLine("Escolha a operação desejada:");
+            Console.WriteLine("1 - Cadastro de Equipamento");
+            Console.WriteLine("2 - Edição de Equipamento");
+            Console.WriteLine("3 - Exclusão de Equipamento");
+            Console.WriteLine("4 - Visualização de Equipamentos");
+            Console.WriteLine("--------------------------------------------");
+
+            Console.Write("Digite um opção válida: ");
+            string opcaoEscolhida = Console.ReadLine();
+
+            return opcaoEscolhida;
+        }
     }
 }
